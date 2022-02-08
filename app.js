0bc6418b0db5a3c70dad4963b68d0d497161321d
mhmtclk1705 <mehmetcelik1705@gmail.com>
2022-02-08 16:00:54 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+let datePicker = document.getElementById("birthday");
+// let calcDate = datePicker.value.getDate();
+console.log(datePicker);
+let loadingImage = document.getElementById("image");
+
+
+function ageCalc(){
+let birthDay = new Date(datePicker.value).getTime();
+let diff = (new Date().getTime() - birthDay) / 1000;
+let minute = 60
+let hour = minute * 60
+let day = hour * 24
+let month = day * 30
+let year = month * 12 + (5*day + 6*hour)
+
+let yearDiff = Math.floor(diff / year);
+let monthDiff = Math.floor((diff %year) / month);
+let dateDiff = Math.floor((diff % month) / day);
+let hourDiff = Math.floor((diff % day) / hour);
+let minuteDiff = Math.floor((diff % hour) / minute);
+let secondDiff = Math.floor((diff % minute));
+
+document.querySelector(".age__calculator--years h2").innerText = `${yearDiff}`
+document.querySelector(".age__calculator--months h2").innerText = `${monthDiff}`
+document.querySelector(".age__calculator--days h2").innerText = `${dateDiff}`
+document.querySelector(".age__calculator--hours h2").innerText = `${hourDiff}`
+document.querySelector(".age__calculator--minutes h2").innerText = `${minuteDiff}`
+document.querySelector(".age__calculator--seconds h2").innerText = `${secondDiff}`
+
+}
+
+datePicker.addEventListener("change", () =>{
+    setInterval(ageCalc,1000)
+    loadingImage.style.opacity = 0});
